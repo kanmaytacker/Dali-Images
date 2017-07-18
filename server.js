8b67f80e0863376bec6551e541ef05036c26c919
kanmaytacker <tanmaykacker40@gmail.com>
2017-07-19 00:44:18 +0530

    Removes stupid mixing of require and import. :')

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 /* eslint no-console: 0 */
 
-const path = require('path');
-const express = require('express');
-const webpack = require('webpack');
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
-const config = require('./webpack.config.js');
+import path from 'path';
+import express from 'express';
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import config from './webpack.config.js';
 import multer from 'multer';
 import imageUploader from './js/middleware/imageUploader';
 
